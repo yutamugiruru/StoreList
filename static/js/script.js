b703901d6ed0e4e56d8013992c99ad9ce3349117
yutamugiruru <ym0133310@gmail.com>
2022-01-26 21:46:45 +0900

    絞り込み検索、検索結果の表示

--- static/js/script.js
+++ static/js/script.js
@@ -10,18 +10,24 @@ $(function () {
     //checkbox
     
     $(function() {
-    let check_list = ".check_list";//検索範囲
-    let list_item = ".article_items";//検索対象
-    let hideItem = "hide_item";//付与クラス
+        let check_list = ".check_list";//検索範囲
+        let list_item = ".article_items";//検索対象
+        let hideItem = "hide_item";//付与クラス
+        var count = 0;
+        let return_count = 0;
+        let default_count = $(list_item).length;
 
     //絞り込み条件の変更
     $(function() {
         $(document).on("change", check_list + " input", function() {
             search_filter();
+            count = default_count - return_count;
+            $('.target_count').text(count)
         });
     });
 
     function search_filter() {
+        return_count = 0;
         $(list_item).removeClass(hideItem);
         for (let i = 0; i < $(check_list).length; i++) {
             let name = $(check_list).eq(i).find("input").attr("name");
@@ -36,9 +42,12 @@ $(function () {
                 let check = array_match_check(itemData, searchData);
                 if (!check) {
                     $(list_item).eq(j).addClass(hideItem);
+                    return_count++;
+                    console.log(return_count);
                 }
             }
         }
+        return return_count;
     }
 
     //チェックの入った値の取得
@@ -53,9 +62,7 @@ $(function () {
     //対象の一覧を取得
     function get_setting_values_in_item(target, data) {
         let itemData = target.data(data);
-        if (!Array.isArray(itemData)) {
-            itemData = [itemData];
-        }
+        
         return itemData;
     }
 
@@ -63,14 +70,16 @@ $(function () {
     function array_match_check(arr1,arr2) {
         let arrCheck = false;
         for (let i = 0; i < arr1.length; i++) {
-            if (arr2.indexOf(arr1[i]) >= 0) {
+            if (arr1.indexOf(arr2[i]) >= 0) {
                 arrCheck = true;
                 break;
             }
         }
         return arrCheck;
     }
-    });
+
+});
+
 
     //destroy_judge
     $(function(){
